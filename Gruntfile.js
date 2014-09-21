abd970f21cd4b26632ebb6b7773d5a12587ed8a1
Carmen V. Krol <carmen.krol@gmail.com>
2014-09-21 17:44:49 -0400

    trying to get local server to work

--- Gruntfile.js
+++ Gruntfile.js
@@ -404,7 +404,7 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('heroku:production', ['clean:server', 'less']);
+  //grunt.registerTask('heroku:production', ['clean:server', 'less']);
 
   grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
