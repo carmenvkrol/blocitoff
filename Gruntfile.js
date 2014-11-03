95e40c0857eff12471cd389981a34017928be2db
Carmen V Krol <carmen.krol@gmail.com>
2014-11-03 12:45:08 -0500

    adding bootstrap glyphicons for heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -343,7 +343,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'fonts/*',
+            'bower_components/bootstrap/dist/*/*'
           ]
         }, {
           expand: true,
